36bafc1b669f569e79b930ed592b6e5685be1f3e
VariousSoil1 <86133234+VariousSoil1@users.noreply.github.com>
2023-03-07 17:38:51 -0500

    fly toggle

--- gamerhub.lua
+++ gamerhub.lua
@@ -1143,6 +1143,19 @@ else
                     loadstring(game:HttpGet(('https://pastebin.com/raw/WxmvCLLH'),true))()
                     flyflag = false
                 elseif not getgenv().flying and not flyflag then
+                    local character = game.Players.LocalPlayer.Character
+                    local oldpos = character.HumanoidRootPart.CFrame
+                    if character:FindFirstChild("Humanoid") then
+                        character["Humanoid"]:ChangeState(15)
+                    end
+                    character:ClearAllChildren()
+                    local newcharacter = Instance.new("Model", workspace)
+                    game.Players.LocalPlayer.Character = newcharacter
+                    wait()
+                    game.Players.LocalPlayer.Character = character
+                    newcharacter:Destroy()
+                    wait()
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = oldpos
                     flyflag = true
                 end
             end)
